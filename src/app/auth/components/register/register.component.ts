bec41089747233ca44690e30bb121e00affeae04
Maria Arkhipova <marias1403@yandex.ru>
2023-12-27 12:55:22 +0900

    refactor: structure

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
 import { registerAction } from '../../store/actions';
+import { isSubmittingSelector } from '../../store/selectors';
 
 @Component({
   selector: 'mc-register',
@@ -10,6 +13,7 @@ import { registerAction } from '../../store/actions';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
 
   constructor(
     private fb: FormBuilder,
@@ -18,8 +22,12 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeValues();
   }
 
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+  }
   initializeForm(): void {
     this.form = this.fb.group({
       username: ['', Validators.required],
