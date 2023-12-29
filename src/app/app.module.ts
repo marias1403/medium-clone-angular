2b48f20951b5f862a8377e15af4f3f1f27d52fe3
Maria Arkhipova <marias1403@yandex.ru>
2023-12-29 15:25:59 +0900

    feat: интерсептор для авторизации

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
@@ -9,6 +9,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { TopBarModule } from './shared/modules/topBar/topBar.module';
+import { PersistenceService } from './shared/services/persistence.service';
+import { AuthInterceptor } from './shared/services/authInterceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,7 +24,14 @@ import { TopBarModule } from './shared/modules/topBar/topBar.module';
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     TopBarModule,
   ],
-  providers: [],
+  providers: [
+    PersistenceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/shared/services/authInterceptor.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { PersistenceService } from './persistence.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private persistenceService: PersistenceService) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> {
+    const token = this.persistenceService.get('accessToken');
+    request = request.clone({
+      setHeaders: {
+        Authorization: token ? `Token ${token}` : '',
+      },
+    });
+    return next.handle(request);
+  }
+}
