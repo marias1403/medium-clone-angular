af717f458acb639fe385bd7924982d96a743bd85
Maria Arkhipova <marias1403@yandex.ru>
2024-01-10 16:06:05 +0900

    feat: стор для фида

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { FeedComponent } from './components/feed/feed.component';
+import { GetFeedEffect } from './store/effects/getFeed.effect';
+import { FeedService } from './services/feed.service';
+import { reducers } from './store/reducers';
 
 @NgModule({
   declarations: [FeedComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([GetFeedEffect]),
+    StoreModule.forFeature('feed', reducers),
+  ],
   exports: [FeedComponent],
+  providers: [FeedService],
 })
 export class FeedModule {}
---
+++ src/app/shared/modules/feed/services/feed.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../../../environments/environment';
+import { GetFeedResponseInterface } from '../types/getFeedResponse.interface';
+
+@Injectable()
+export class FeedService {
+  constructor(private http: HttpClient) {}
+
+  getFeed(url: string): Observable<GetFeedResponseInterface> {
+    const fullUrl = environment.apiUrl + url;
+    return this.http.get<GetFeedResponseInterface>(fullUrl);
+  }
+}
