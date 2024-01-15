365d9ec999bbc28ad90401bcd6031538e81b1ee8
Maria Arkhipova <marias1403@yandex.ru>
2024-01-15 15:44:24 +0900

    feat: переключатель между фидами

--- src/app/globalFeed/globalFeed.module.ts
+++ src/app/globalFeed/globalFeed.module.ts
@@ -6,6 +6,7 @@ import { GlobalFeedComponent } from './components/global-feed/global-feed.compon
 import { FeedModule } from '../shared/modules/feed/feed.module';
 import { BannerModule } from '../shared/modules/banner/banner.module';
 import { PopularTagsModule } from '../shared/modules/popularTags/popularTags.module';
+import { FeedTogglerModule } from '../shared/modules/feedToggler/feedToggler.module';
 
 const routes = [
   {
@@ -22,6 +23,7 @@ const routes = [
     FeedModule,
     BannerModule,
     PopularTagsModule,
+    FeedTogglerModule,
   ],
 })
 export class GlobalFeedModule {}
